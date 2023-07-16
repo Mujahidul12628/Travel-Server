2046839403565618f934f4e69f6d9e55a2a41b8e
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-07-16 23:32:06 +0600

    Travel Places API added

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ const port = process.env.PORT || 5012;
 
 app.use(cors());
 
+const travelPlaces = require("./travelPlaces.json");
+
 
 
 app.get('/', (req, res) => {
     res.send('Travel Server is running');
 });
 
-
-
+app.get('/travelPlaces', (req, res) => {
+    res.send(travelPlaces);
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
